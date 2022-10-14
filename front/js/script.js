b671bbe95b6cabfcca1a9b63cb6595bff00d2255
JEREMY 06130 <103517138+Jeremy06130@users.noreply.github.com>
2022-10-14 12:02:59 +0200

    mise a jour fichier JS

--- front/js/script.js
+++ front/js/script.js
@@ -1,42 +1,48 @@
-//------------------------------------------------------------------------
-// Récupération des produits de l'api
-fetch("http://localhost:3000/api/products")
+fillSection();
 
-  // quand tu as la réponse donne le résultat en json.
-  .then((res) => res.json())
+// Récupération des articles de l'API
+async function getArticles() {
+    var articlesCatch = await fetch("http://localhost:3000/api/products")
+    return await articlesCatch.json();
+}
 
-  // ce que l'on a reçu et qui a été traité en json sera appelé objetProduits
-  .then((objetProduits) => {
-    // donne moi des informations en console sur ce qui est récupéré sous forme tableau.
-    console.table(objetProduits);
-    // appel de la fonction d'affichage des produits
-    lesKanaps(objetProduits);
-  })
+    // Répartition des données de l'API dans le DOM
+async function fillSection() {
+    var result = await getArticles ()
+    .then(function (resultatAPI){
+        const articles = resultatAPI;
+        console.table(articles);
+        for (let article in articles) {
 
-  // dans le cas d'une erreur remplace le contenu de titre par un h1 au contenu de erreur 404 et renvoit en console l'erreur.
-  .catch((err) => {
-    document.querySelector(".titles").innerHTML = "<h1>erreur 404</h1>";
-    console.log("erreur 404, sur ressource api:" + err);
-  });
+            // Insertion de l'élément "a"
+            let productLink = document.createElement("a");
+            document.querySelector(".items").appendChild(productLink);
+            productLink.href = `product.html?id=${resultatAPI[article]._id}`;
 
+            // Insertion de l'élément "article"
+            let productArticle = document.createElement("article");
+            productLink.appendChild(productArticle);
 
-// fonction d'affichage des produits de l'api sur la page index
-function lesKanaps(index) {
-    
-  // déclaration de variable de la zone d'article
-  let zoneArticle = document.querySelector("#items");
+            // Insertion de l'image
+            let productImg = document.createElement("img");
+            productArticle.appendChild(productImg);
+            productImg.src = resultatAPI[article].imageUrl;
+            productImg.alt = resultatAPI[article].altTxt;
 
-  // boucle pour chaque indice(nommé 'article') dans index
-  for (let article of index) {
-    /* création et ajout des zones d'articles, insertion de l'adresse produit via chemin produit + paramètres(son id);
-    la page index est http://127.0.0.1:5500/front/html/index.html donc la page du produit sera http://127.0.0.1:5500/front/html/product.html 
-    (d'ou le ./product.html) pour rajouter son paramètre on met ? puis la clé (ici _id) associé (=) à sa valeur dynamique ${article._id} */
-    zoneArticle.inner += `<a href="./product.html?_id=${article._id}">
-    <article>
-      <img src="${article.imageUrl}" alt="${article.altTxt}">
-      <h3 class="productName">${article.name}</h3>
-      <p class="productDescription">${article.description}</p>
-    </article>
-  </a>`;
-  }
+            // Insertion du titre "h3"
+            let productName = document.createElement("h3");
+            productArticle.appendChild(productName);
+            productName.classList.add("productName");
+            productName.innerHTML = resultatAPI[article].name;
+
+            // Insertion de la description "p"
+            let productDescription = document.createElement("p");
+            productArticle.appendChild(productDescription);
+            productDescription.classList.add("productName");
+            productDescription.innerHTML = resultatAPI[article].description;
+        }
+    })
+    .catch (function(error){
+        return error;
+    });
 }
